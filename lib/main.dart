f3158313ac6e63da138e021da3c78c67bb064240
romankirpichnikov <fcv3@yandex.com>
2023-02-13 23:48:27 +0300

    replace home widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,77 +14,23 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyFirstWidget(),
+      home: MyFirstWidget(
+        title: 'Hello!',
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyFirstWidget extends StatelessWidget {
   final String title;
 
-  const MyHomePage({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  MyFirstWidget({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-}
-
-class MyFirstWidget extends StatefulWidget {
-  MyFirstWidget({super.key});
-
-  @override
-  State<MyFirstWidget> createState() => _MyFirstWidgetState();
-}
-
-class _MyFirstWidgetState extends State<MyFirstWidget> {
-  late int _counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    _counter++;
-    print('Counter: $_counter');
-
     return Container(
-      child: const Center(
-        child: Text('Hello!'),
+      child: Center(
+        child: Text(title),
       ),
     );
   }
