ff95e6567de7b4e5f0e936ff62f80339f0a623dc
romankirpichnikov <fcv3@yandex.com>
2023-02-18 15:24:27 +0300

    add scafold

--- lib/ui/sight_list_screen.dart
+++ lib/ui/sight_list_screen.dart
@@ -10,6 +10,6 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
