6c682f54bcd24ee2c2016450ab37434cb47b5000
romankirpichnikov <fcv3@yandex.com>
2023-02-20 23:53:25 +0300

    some rafactor

--- lib/ui/sight_list_screen.dart
+++ lib/ui/sight_list_screen.dart
@@ -12,16 +12,18 @@ class _SightListScreenState extends State<SightListScreen> {
 
   @override
   Widget build(BuildContext context) {
+    const appBarTextStyle = TextStyle(
+      color: Colors.black,
+      fontFamily: 'Roboto',
+      fontWeight: FontWeight.w700,
+      fontSize: 32,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: RichText(
           text: const TextSpan(
-            style: TextStyle(
-              color: Colors.black,
-              fontFamily: 'Roboto',
-              fontWeight: FontWeight.w700,
-              fontSize: 32,
-            ),
+            style: appBarTextStyle,
             children: [
               TextSpan(
                 text: 'С',
@@ -30,17 +32,17 @@ class _SightListScreenState extends State<SightListScreen> {
               TextSpan(text: 'писок\n'),
               TextSpan(
                 text: 'и',
-                style: TextStyle(color: Color.fromRGBO(37, 40, 73, 1)),
+                style: TextStyle(color: Color.fromRGBO(59, 62, 91, 1)),
               ),
               TextSpan(text: 'нтересных мест'),
             ],
           ),
         ),
         centerTitle: false,
-        backgroundColor: _backgroundColor,
         elevation: 0,
         toolbarHeight: 64,
         titleSpacing: 16,
+        backgroundColor: _backgroundColor,
       ),
       backgroundColor: _backgroundColor,
     );
