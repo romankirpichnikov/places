ecb3237f9156cfd093366d27277878662a231af3
romankirpichnikov <fcv3@yandex.com>
2023-03-05 20:00:21 +0300

    add sight cards

---
+++ lib/ui/sight_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:places/constants/domain/sight_types.dart';
+import 'package:places/domain/sight.dart';
+
+class SightCard extends StatelessWidget {
+  final Sight sight;
+
+  const SightCard({super.key, required this.sight});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 20, top: 20),
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(15)),
+          color: Color(0xFFF5F5F5),
+        ),
+        clipBehavior: Clip.hardEdge,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              color: Colors.indigo,
+              height: 100,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          sight.type.name,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                        Container(
+                          width: 20,
+                          height: 20,
+                          color: Colors.redAccent,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    sight.name,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    sight.details,
+                    maxLines: 2,
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
