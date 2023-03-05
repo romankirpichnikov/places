c11e3ab3eb2404ac0a2ec94af00aedeec661d9b4
romankirpichnikov <fcv3@yandex.com>
2023-03-05 20:00:21 +0300

    add sight cards

--- lib/constants/domain/sight_types.dart
+++ lib/constants/domain/sight_types.dart
@@ -1,1 +1,29 @@
-enum SightType { architecture, history, culture, art, nature, other }
+enum SightType {
+  architecture,
+  history,
+  culture,
+  art,
+  nature,
+  other,
+}
+
+extension SightTypeExtension on SightType {
+  String get name {
+    switch (this) {
+      case SightType.architecture:
+        return 'Архитектура';
+      case SightType.history:
+        return 'История';
+      case SightType.culture:
+        return 'Культура';
+      case SightType.art:
+        return 'Искуство';
+      case SightType.nature:
+        return 'Природа';
+      case SightType.other:
+        return 'Другое';
+      default:
+        return 'Другое';
+    }
+  }
+}
