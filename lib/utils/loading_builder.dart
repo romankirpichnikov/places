6296ef9dc01bd76d0cc8d74adeb2868b6178e147
romankirpichnikov <fcv3@yandex.com>
2023-03-09 01:00:17 +0300

    add text progress to loader

--- lib/utils/loading_builder.dart
+++ lib/utils/loading_builder.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const double kLoadingProgressIndicatorSize = 24.0;
+const int kDefaultExpectedTotalBytes = -1;
+
 Widget loadingBuilder(
   BuildContext context,
   Widget child,
@@ -9,12 +12,21 @@ Widget loadingBuilder(
     return child;
   }
 
+  final expectedTotalBytes = loadingProgress.expectedTotalBytes;
+  final progress = loadingProgress.cumulativeBytesLoaded / expectedTotalBytes!;
+  final loadedPercent = (progress * 100).toInt();
+
   return Center(
-    child: CircularProgressIndicator(
-      value: loadingProgress.expectedTotalBytes != null
-          ? loadingProgress.cumulativeBytesLoaded /
-              loadingProgress.expectedTotalBytes!
-          : null,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircularProgressIndicator(
+          value: progress,
+          strokeWidth: kLoadingProgressIndicatorSize / 4,
+        ),
+        const SizedBox(height: 8),
+        Text('Загрузка $loadedPercent %'),
+      ],
     ),
   );
 }
